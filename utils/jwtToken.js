df6e8fa10f13457cf3211e3f898f54e0823392ea
T_Himanshu_Sharma <115513580+Himanshu07-debug@users.noreply.github.com>
2024-04-26 10:31:06 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,11 +1,26 @@
 export const sendToken = (user, statusCode, message, res) => {
-  const token = user.getJWTToken();
+
+  const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+  };
+
+  let token = jwt.sign(payload,
+                                process.env.JWT_SECRET_KEY,
+                                {
+                                    expiresIn:"2h",
+  
+  });
+  
+  // const token = user.getJWTToken();
   const options = {
     expires: new Date(
       Date.now() + 7*24*60*60*1000
     ),
     httpOnly: true,
   };
+  
   res.status(200).cookie("token", token, options).json({
     success: true,
     user,
