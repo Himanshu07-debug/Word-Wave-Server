6984b746b37144e89db2fefe585fa829a6baf62e
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-04-26 07:23:48 +0530

    modification in server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 6010;
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
